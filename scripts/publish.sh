6be61dd298bcec4a0ce640baee452ef376b88b0b
Michael Boyarov <mi6gan@mail.ru>
2019-09-02 20:37:13 +0300

    #142 Split modules to monorepo packages

--- scripts/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-{ echo "$NPM_USERNAME"; sleep 1; echo "$NPM_EMAIL"; } | yarn login &&
-yarn config set //registry.npmjs.org/:_authToken "$NPM_TOKEN" &&
-yarn workspaces run publish --access public --patch &&
-yarn config delete //registry.npmjs.org/:_authToken
