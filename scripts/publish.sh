645116a57d0e1bfb88704d78664c706d683297ea
Mikhail Boyarov <mi6gan@mail.ru>
2019-09-02 17:05:24 +0000

    testbox/bream#441 Pie-диаграмы в прохождении и ревью теста
    * remove global styles

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,5 @@
 
 { echo "$NPM_USERNAME"; sleep 1; echo "$NPM_EMAIL"; } | yarn login &&
 yarn config set //registry.npmjs.org/:_authToken "$NPM_TOKEN" &&
-yarn workspaces run publish --access public --new-version "$CI_COMMIT_TAG" &&
+yarn workspaces run publish --access public --patch &&
 yarn config delete //registry.npmjs.org/:_authToken
