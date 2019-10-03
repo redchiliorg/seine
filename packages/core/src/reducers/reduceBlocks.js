5be387ffe239a2765a5d9f58d1541f9135779edb
Michael Boyarov <mi6gan@mail.ru>
2019-10-03 19:54:27 +0300

    #176 Относительное добавление блоков
    * add relative block creation actions

--- packages/core/src/reducers/reduceBlocks.js
+++ packages/core/src/reducers/reduceBlocks.js
@@ -1,5 +1,6 @@
 // @flow
 import { equals } from 'ramda';
+import { createBlock } from '@seine/core';
 
 import type { Block, BlockBody, BlockFormat, BlockId } from '../types';
 import { blockTypes } from '../types';
@@ -19,6 +20,34 @@ export const initialBlocksState: BlocksState = {
 export const CREATE_BLOCK = '@seine/core/createBlock';
 export type CreateBlockAction = {
   type: typeof CREATE_BLOCK,
+  block: $Shape<Block>,
+};
+
+export const CREATE_BOTTOM_BLOCK = '@seine/core/createBottomBlock';
+export type CreateBottomBlockAction = {
+  type: typeof CREATE_BOTTOM_BLOCK,
+  id: BlockId,
+  block: $Shape<Block>,
+};
+
+export const CREATE_LEFT_BLOCK = '@seine/core/createLeftBlock';
+export type CreateLeftBlockAction = {
+  type: typeof CREATE_LEFT_BLOCK,
+  id: BlockId,
+  block: $Shape<Block>,
+};
+
+export const CREATE_RIGHT_BLOCK = '@seine/core/createRightBlock';
+export type CreateRightBlockAction = {
+  type: typeof CREATE_RIGHT_BLOCK,
+  id: BlockId,
+  block: $Shape<Block>,
+};
+
+export const CREATE_TOP_BLOCK = '@seine/core/createTopBlock';
+export type CreateTopBlockAction = {
+  type: typeof CREATE_TOP_BLOCK,
+  id: BlockId,
   block: $Shape<Block>,
 };
 
@@ -60,6 +89,10 @@ export type UpdateBlockEditorAction = {
 
 export type BlocksAction =
   | CreateBlockAction
+  | CreateBottomBlockAction
+  | CreateLeftBlockAction
+  | CreateRightBlockAction
+  | CreateTopBlockAction
   | DeleteSelectedBlocksAction
   | SelectBlockAction
   | UpdateBlockDataAction
@@ -77,6 +110,65 @@ export function reduceBlocks(
   action: BlocksAction
 ): BlocksState {
   switch (action.type) {
+    case CREATE_BLOCK:
+      return {
+        ...state,
+        blocks: [...state.blocks, action.block],
+      };
+
+    case CREATE_BOTTOM_BLOCK: {
+      const index = state.blocks.findIndex(({ id }) => id === action.id);
+      return {
+        ...state,
+        blocks: [
+          ...state.blocks.slice(0, index + 1),
+          action.block,
+          ...state.blocks.slice(index + 1),
+        ],
+      };
+    }
+
+    case CREATE_TOP_BLOCK: {
+      const index = state.blocks.findIndex(({ id }) => id === action.id);
+      return {
+        ...state,
+        blocks: [
+          ...state.blocks.slice(0, index),
+          action.block,
+          ...state.blocks.slice(index),
+        ],
+      };
+    }
+
+    case CREATE_LEFT_BLOCK:
+    case CREATE_RIGHT_BLOCK: {
+      const index = state.blocks.findIndex(({ id }) => id === action.id);
+      if (index === -1) {
+        return state;
+      }
+      const parent =
+        state.blocks.find(
+          ({ id, type }) =>
+            id === state.blocks[index].parent_id && type === blockTypes.GRID
+        ) ||
+        createBlock(
+          blockTypes.GRID,
+          null,
+          null,
+          state.blocks[index]['parent_id']
+        );
+      return {
+        ...state,
+        blocks: [
+          ...state.blocks.slice(0, index),
+          parent,
+          ...(action.type === CREATE_LEFT_BLOCK
+            ? [action.block, ...state.blocks.slice(index + 1)]
+            : [...state.blocks.slice(index + 1), action.block]),
+        ],
+      };
+    }
+
     case SELECT_BLOCK: {
       const index = state.selection.indexOf(action.id);
 
@@ -108,26 +200,31 @@ export function reduceBlocks(
       }
     }
 
-    case CREATE_BLOCK:
-      return {
-        ...state,
-        blocks: [...state.blocks, action.block],
-      };
-
     case DELETE_SELECTED_BLOCKS:
       if (state.selection.length === 0) {
         return state;
       }
+      const blocks = state.blocks.filter(
+        ({ id }) => !state.selection.includes(id)
+      );
+      const redundant = blocks.filter(
+        ({ id, type }, _, blocks) =>
+          type === blockTypes.GRID &&
+          blocks.filter(({ parent_id }) => parent_id === id).length < 2
+      );
       return {
         ...state,
         selection: [],
-        blocks: state.blocks
-          .filter(({ id }) => !state.selection.includes(id))
-          .filter(
-            ({ id, type }, _, blocks) =>
-              type !== blockTypes.GRID ||
-              blocks.some(({ parent_id }) => parent_id === id)
-          ),
+        blocks: blocks
+          .filter((block) => !redundant.includes(block))
+          .map((block) => {
+            const redundantParent = redundant.find(
+              ({ id }) => block.parent_id === id
+            );
+            return redundantParent
+              ? { ...block, parent_id: redundantParent.parent_id }
+              : block;
+          }),
       };
 
     case UPDATE_BLOCK_BODY:
