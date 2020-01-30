c00692c8689fabf68c95e4d2a503b3b14944fde4
Mikhail Boyarov <mi6gan@mail.ru>
2020-01-30 22:53:59 +0300

    seine-92 Таблицы: ховер меню блока

--- packages/tables-editor/src/TableEditor.js
+++ packages/tables-editor/src/TableEditor.js
@@ -10,10 +10,16 @@ import type { TableProps } from '@seine/tables';
 import { Table } from '@seine/tables';
 import styled from 'styled-components/macro';
 import { useAutoCallback } from 'hooks.macro';
+import { BlockActions } from '@seine/ui';
 
 import { defaultEditor } from './constants';
 
 type Props = TableProps & BlockEditor;
+
+const Container = styled.div`
+  position: relative;
+  height: 100%;
+`;
 
 const StyledInput = styled.input`
   && {
@@ -34,71 +40,90 @@ const StyledInput = styled.input`
  * @param {Props} props
  * @returns {React.Node}
  */
-export default function TableEditor({ id, dispatch, header, rows }: Props) {
+export default function TableEditor({
+  addButtonRenderMap,
+  id,
+  dispatch,
+  header,
+  rows,
+  selection,
+}: Props) {
   const selectHeader = useAutoCallback(() => {
     dispatch({ id, type: SELECT_BLOCK });
     dispatch({ id, type: UPDATE_BLOCK_EDITOR, editor: defaultEditor });
   });
 
   return (
-    <Table
-      header={header.map(({ text, ...column }, index) => ({
-        ...column,
-        text: (
-          <StyledInput
-            onFocus={selectHeader}
-            onChange={({ currentTarget }) =>
-              dispatch({
-                id,
-                type: UPDATE_BLOCK_BODY,
-                body: {
-                  header: [
-                    ...header.slice(0, index),
-                    { ...column, text: currentTarget.value },
-                    ...header.slice(index + 1),
-                  ],
-                },
-              })
-            }
-            value={text}
-          />
-        ),
-      }))}
-      rows={rows.map((row, rowIndex) =>
-        row.map(({ text, ...column }, columnIndex) => ({
-          ...column,
-          text: (
-            <StyledInput
-              onFocus={() => {
-                dispatch({ id, type: SELECT_BLOCK });
-                dispatch({
-                  id,
-                  type: UPDATE_BLOCK_EDITOR,
-                  editor: { columnIndex, rowIndex },
-                });
-              }}
-              onChange={({ currentTarget }) =>
-                dispatch({
-                  id,
-                  type: UPDATE_BLOCK_BODY,
-                  body: {
-                    rows: [
-                      ...rows.slice(0, rowIndex),
-                      [
-                        ...row.slice(0, columnIndex),
+    <Container>
+      {selection.length === 1 && selection[0] === id ? (
+        <Table
+          header={header.map(({ text, ...column }, index) => ({
+            ...column,
+            text: (
+              <StyledInput
+                onFocus={selectHeader}
+                onChange={({ currentTarget }) =>
+                  dispatch({
+                    id,
+                    type: UPDATE_BLOCK_BODY,
+                    body: {
+                      header: [
+                        ...header.slice(0, index),
                         { ...column, text: currentTarget.value },
-                        ...row.slice(columnIndex + 1),
+                        ...header.slice(index + 1),
                       ],
-                      ...rows.slice(rowIndex + 1),
-                    ],
-                  },
-                })
-              }
-              value={text}
-            />
-          ),
-        }))
+                    },
+                  })
+                }
+                value={text}
+              />
+            ),
+          }))}
+          rows={rows.map((row, rowIndex) =>
+            row.map(({ text, ...column }, columnIndex) => ({
+              ...column,
+              text: (
+                <StyledInput
+                  onFocus={() => {
+                    dispatch({ id, type: SELECT_BLOCK });
+                    dispatch({
+                      id,
+                      type: UPDATE_BLOCK_EDITOR,
+                      editor: { columnIndex, rowIndex },
+                    });
+                  }}
+                  onChange={({ currentTarget }) =>
+                    dispatch({
+                      id,
+                      type: UPDATE_BLOCK_BODY,
+                      body: {
+                        rows: [
+                          ...rows.slice(0, rowIndex),
+                          [
+                            ...row.slice(0, columnIndex),
+                            { ...column, text: currentTarget.value },
+                            ...row.slice(columnIndex + 1),
+                          ],
+                          ...rows.slice(rowIndex + 1),
+                        ],
+                      },
+                    })
+                  }
+                  value={text}
+                />
+              ),
+            }))
+          )}
+        />
+      ) : (
+        <Table header={header} rows={rows} />
       )}
-    />
+      <BlockActions
+        addButtonRenderMap={addButtonRenderMap}
+        dispatch={dispatch}
+        id={id}
+        selection={selection}
+      />
+    </Container>
   );
 }
